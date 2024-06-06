e1be6d649a7af27559424efd07afcacb71e55bac
takuyaEngineer <engineer.memorandum@gmail.com>
2024-06-06 12:20:46 +0900

    とりあえずクエリを実行できるようになった

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,11 +1,24 @@
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
-
 import { PrismaModule } from 'nestjs-prisma';
+import { GraphQLModule } from '@nestjs/graphql';
+import { ApolloDriver, ApolloDriverConfig } from '@nestjs/apollo';
+import { UsersModule } from './modules/users/users.module';
+import { PostsModule } from './modules/posts/posts.module';
 
 @Module({
-  imports: [PrismaModule.forRoot({ isGlobal: true })],
+  imports: [
+    GraphQLModule.forRoot<ApolloDriverConfig>({
+      driver: ApolloDriver,
+      //   debug: true,
+      playground: true,
+      autoSchemaFile: './src/schema.graphql',
+    }),
+    PrismaModule.forRoot({ isGlobal: true }),
+    UsersModule,
+    PostsModule,
+  ],
   controllers: [AppController],
   providers: [AppService],
 })
